69146e6e68f3b38ab9aec1a79fbfa829cdba013f
Oluwatofunmi Caulcrick <johncaul@yahoo.com>
2026-04-08 15:30:05 +0100

    Changed version number to 0.1.1

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='pymongo_shard',
-      version='0.1.0',
+      version='0.1.1',
       description ='A package to extract data via API requuests from server shards (endpoints) running pymongo, the official Python MongoDB driver.',
       packages=['pymongo_shard'],
       python_requires='>=3.11, !=4.0.*',
